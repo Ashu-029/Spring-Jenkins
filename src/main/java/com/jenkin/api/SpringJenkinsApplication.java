4c1e2c1e3c73a9425c1710ab841541231aa5c4bb
Ashu-029 <ashishsawale2902@gmail.com>
2023-07-15 16:29:29 +0530

    First Commit

---
+++ src/main/java/com/jenkin/api/SpringJenkinsApplication.java
@@ -0,0 +1,30 @@
+package com.jenkin.api;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	
+	private static final Logger log = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+	@PostConstruct
+	public void init() {
+		
+		log.info(" Application Has Started......... ");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		log.info(" Application Has Started......... ");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	
+	}
+
+}
